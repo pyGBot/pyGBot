pyGBot/pyGBot

commit f18a401fad06b2e113a10cc653b2d45ed6d049af

Use replyout in all CommandSpecs

pyGBot/Plugins/system/CommandSpec/Seen.py
@@ -36,16 +36,9 @@ class Seen(BaseCommand):
     level = AL.User
 
     def __init__(self, bot, channel, user, args):
-        if channel is None:
-            out = privout(bot)
-            target = user
-        else:
-            out = pubout(bot)
-            target = channel
-
         args = args.strip().split()
         if not args:
-            out(target, 'Command usage: seen <user> [channel]')
+            replyout(channel, user, 'Command usage: seen <user> [channel]')
             return
 
         username = args[0]
@@ -106,5 +99,5 @@ class Seen(BaseCommand):
             outmessage += "changing nick to %s." % event.message
         elif event.type == "NickFrom":
             outmessage += "changing nick from %s." % event.message
-        out(target, outmessage)
+        replyout(channel, user, outmessage)
 

pyGBot/Plugins/system/CommandSpec/StatsRoll.py
@@ -41,22 +41,15 @@ class StatsRoll(BaseCommand):
 
         rolls,stats = self.rollStats(sortdir)
 
-        if channel is None:
-            out = bot.privout
-            target = user
-        else:
-            out = bot.pubout
-            target = channel
-
         if verbose:
             if sortdir == 'asc':
                 rolls.sort(key=sum)
             elif sortdir == 'desc':
                 rolls.sort(key=sum, reverse=True)
 
-            out(target,str(rolls))
+            replyout(channel, user, str(rolls))
 
-        out(target,"Stats are: %s." % ', '.join(map(str, stats)))
+        replyout(channel, user, ,"Stats are: %s." % ', '.join(map(str, stats)))
 
 
     def rollStats(self, sortdir):

pyGBot/Plugins/system/CommandSpec/StopGame.py
@@ -25,4 +25,4 @@ class StopGame(BaseCommand):
         for p in bot.activeplugins:
             if p.startswith('games.'):
                 if bot.deactivatePlugin(p, channel):
-                    bot.pubout(channel, "%s stopped." % p.split('.')[1])
+                    bot.replyout(channel, user, "%s stopped." % p.split('.')[1])

pyGBot/Plugins/system/CommandSpec/Wiki.py
@@ -28,21 +28,16 @@ import json
 class Wiki(BaseCommand):
     level = AL.User
     def __init__(self, bot, channel, user, args):
-        if channel is None:
-            out = bot.privout
-            target = user
-        else:
-            out = bot.pubout
-            target = channel
-            
         search = "".join(args)
         existurl = "http://en.wikipedia.org/w/api.php?action=query&titles=%s&format=json" % search.replace(" ", "%20")
         url = "http://en.wikipedia.org/wiki/%s" % search.replace(" ", "%20")
 
         if self.check_wiki_exists(existurl):
-            out(target,"URL for Wikipedia - %s: %s." % (search, self.tiny_url(url)))
+            replyout(channel, user, "URL for Wikipedia - %s: %s." %
+                                        (search, self.tiny_url(url)))
         else:
-            out(target,"Sorry, no Wikipedia page exists for '%s'." % search) 
+            replyout(channel, user, 
+                     "Sorry, no Wikipedia page exists for '%s'." % search) 
 
     def check_wiki_exists(self, url):
         page = json.loads(urlopen(url).readline())

pyGBot/Plugins/system/CommandSpec/xkcdWiki.py
@@ -28,21 +28,16 @@ import json
 class xkcdWiki(BaseCommand):
     level = AL.User
     def __init__(self, bot, channel, user, args):
-        if channel is None:
-            out = bot.privout
-            target = user
-        else:
-            out = bot.pubout
-            target = channel
-            
         search = "".join(args)
         existurl = "http://wiki.xkcd.com/wirc/api.php?action=query&titles=%s&format=json" % search.replace(" ", "%20")
         url = "http://wiki.xkcd.com/irc/%s" % search.replace(" ", "%20")
 
         if self.check_wiki_exists(existurl):
-            out(target,"URL for XKCDWiki - %s: %s." % (search, self.tiny_url(url)))
+            replyout(channel, user, "URL for XKCDWiki - %s: %s." %
+                                        (search, self.tiny_url(url)))
         else:
-            out(target,"Sorry, no XKCDWiki page exists for '%s'." % search)
+            replyout(channel, user, 
+                     "Sorry, no XKCDWiki page exists for '%s'." % search)
 
     def check_wiki_exists(self, url):
         page = json.loads(urlopen(url).readline())

commit 97dff4fa38ad1d8732e971d0f79b976b20161e7b

Undid changes in f18a401fad06b2e113a10cc653b2d45ed6d049af which broke a number of CommandSpecs.

pyGBot/Plugins/system/CommandSpec/Seen.py
@@ -36,9 +36,16 @@ class Seen(BaseCommand):
     level = AL.User
 
     def __init__(self, bot, channel, user, args):
+        if channel is None:
+            out = privout(bot)
+            target = user
+        else:
+            out = pubout(bot)
+            target = channel
+
         args = args.strip().split()
         if not args:
-            replyout(channel, user, 'Command usage: seen <user> [channel]')
+            out(target, 'Command usage: seen <user> [channel]')
             return
 
         username = args[0]
@@ -99,5 +106,5 @@ class Seen(BaseCommand):
             outmessage += "changing nick to %s." % event.message
         elif event.type == "NickFrom":
             outmessage += "changing nick from %s." % event.message
-        replyout(channel, user, outmessage)
+        out(target, outmessage)
 

pyGBot/Plugins/system/CommandSpec/StatsRoll.py
@@ -41,15 +41,22 @@ class StatsRoll(BaseCommand):
 
         rolls,stats = self.rollStats(sortdir)
 
+        if channel is None:
+            out = bot.privout
+            target = user
+        else:
+            out = bot.pubout
+            target = channel
+
         if verbose:
             if sortdir == 'asc':
                 rolls.sort(key=sum)
             elif sortdir == 'desc':
                 rolls.sort(key=sum, reverse=True)
 
-            replyout(channel, user, str(rolls))
+            out(target,str(rolls))
 
-        replyout(channel, user, "Stats are: %s." % ', '.join(map(str, stats)))
+        out(target,"Stats are: %s." % ', '.join(map(str, stats)))
 
 
     def rollStats(self, sortdir):

pyGBot/Plugins/system/CommandSpec/StopGame.py
@@ -25,4 +25,4 @@ class StopGame(BaseCommand):
         for p in bot.activeplugins:
             if p.startswith('games.'):
                 if bot.deactivatePlugin(p, channel):
-                    bot.replyout(channel, user, "%s stopped." % p.split('.')[1])
+                    bot.pubout(channel, "%s stopped." % p.split('.')[1])

pyGBot/Plugins/system/CommandSpec/Wiki.py
@@ -28,16 +28,21 @@ import json
 class Wiki(BaseCommand):
     level = AL.User
     def __init__(self, bot, channel, user, args):
+        if channel is None:
+            out = bot.privout
+            target = user
+        else:
+            out = bot.pubout
+            target = channel
+            
         search = "".join(args)
         existurl = "http://en.wikipedia.org/w/api.php?action=query&titles=%s&format=json" % search.replace(" ", "%20")
         url = "http://en.wikipedia.org/wiki/%s" % search.replace(" ", "%20")
 
         if self.check_wiki_exists(existurl):
-            replyout(channel, user, "URL for Wikipedia - %s: %s." %
-                                        (search, self.tiny_url(url)))
+            out(target,"URL for Wikipedia - %s: %s." % (search, self.tiny_url(url)))
         else:
-            replyout(channel, user, 
-                     "Sorry, no Wikipedia page exists for '%s'." % search) 
+            out(target,"Sorry, no Wikipedia page exists for '%s'." % search) 
 
     def check_wiki_exists(self, url):
         page = json.loads(urlopen(url).readline())

pyGBot/Plugins/system/CommandSpec/xkcdWiki.py
@@ -28,16 +28,21 @@ import json
 class xkcdWiki(BaseCommand):
     level = AL.User
     def __init__(self, bot, channel, user, args):
+        if channel is None:
+            out = bot.privout
+            target = user
+        else:
+            out = bot.pubout
+            target = channel
+            
         search = "".join(args)
         existurl = "http://wiki.xkcd.com/wirc/api.php?action=query&titles=%s&format=json" % search.replace(" ", "%20")
         url = "http://wiki.xkcd.com/irc/%s" % search.replace(" ", "%20")
 
         if self.check_wiki_exists(existurl):
-            replyout(channel, user, "URL for XKCDWiki - %s: %s." %
-                                        (search, self.tiny_url(url)))
+            out(target,"URL for XKCDWiki - %s: %s." % (search, self.tiny_url(url)))
         else:
-            replyout(channel, user, 
-                     "Sorry, no XKCDWiki page exists for '%s'." % search)
+            out(target,"Sorry, no XKCDWiki page exists for '%s'." % search)
 
     def check_wiki_exists(self, url):
         page = json.loads(urlopen(url).readline())